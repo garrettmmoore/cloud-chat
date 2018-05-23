ff29cbb958892b87e217cfacea73387906c6ff12
Garrett Moore <garrettmarkmoore@gmail.com>
2018-05-22 17:11:31 -0700

    Update code for horizontal autoscaling and implement socket.io-redis adapter

--- index.js
+++ index.js
@@ -4,6 +4,15 @@ var app = express();
 var path = require('path');
 var server = require('http').createServer(app);
 var io = require('socket.io')(server);
+var redis = require('socket.io-redis');
+io.adapter(redis({ host: process.env.REDIS_ENDPOINT, port: 6379 }));
+
+var Presence = require('./lib/presence');
+
+// Lower the heartbeat timeout
+io.set('heartbeat timeout', 8000);
+io.set('heartbeat interval', 4000);
+
 var port = process.env.PORT || 3000;
 
 server.listen(port, function () {
@@ -12,10 +21,6 @@ server.listen(port, function () {
 
 // Routing
 app.use(express.static(path.join(__dirname, 'public')));
-
-// Chatroom
-
-var numUsers = 0;
 
 io.on('connection', function (socket) {
   var addedUser = false;
@@ -29,21 +34,40 @@ io.on('connection', function (socket) {
     });
   });
 
+  socket.conn.on('heartbeat', function () {
+    if (!addedUser) {
+      // Don't start upserting until the user has added themselves.
+      return;
+    }
+
+    Presence.upsert(socket.id, {
+      username: socket.username
+    });
+  });
+
   // when the client emits 'add user', this listens and executes
   socket.on('add user', function (username) {
-    if (addedUser) return;
+    if (addedUser) {
+      return;
+    }
 
     // we store the username in the socket session for this client
     socket.username = username;
-    ++numUsers;
-    addedUser = true;
-    socket.emit('login', {
-      numUsers: numUsers
+    Presence.upsert(socket.id, {
+      username: socket.username
     });
-    // echo globally (all clients) that a person has connected
-    socket.broadcast.emit('user joined', {
-      username: socket.username,
-      numUsers: numUsers
+    addedUser = true;
+
+    Presence.list(function (users) {
+      socket.emit('login', {
+        numUsers: users.length
+      });
+
+      // echo globally (all clients) that a person has connected
+      socket.broadcast.emit('user joined', {
+        username: socket.username,
+        numUsers: users.length
+      });
     });
   });
 
@@ -64,12 +88,14 @@ io.on('connection', function (socket) {
   // when the user disconnects.. perform this
   socket.on('disconnect', function () {
     if (addedUser) {
-      --numUsers;
+      Presence.remove(socket.id);
 
-      // echo globally that this client has left
-      socket.broadcast.emit('user left', {
-        username: socket.username,
-        numUsers: numUsers
+      Presence.list(function (users) {
+        // echo globally (all clients) that a person has connected
+        socket.broadcast.emit('user left', {
+          username: socket.username,
+          numUsers: users.length
+        });
       });
     }
   });
